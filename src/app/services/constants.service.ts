455c7540da0f85819bf52d9bb08df75b282b996e
Nik Nauwelaerts <Nik Nauwelaerts>
2017-02-06 16:04:02 +0100

    First Codeship build

--- src/app/services/constants.service.ts
+++ src/app/services/constants.service.ts
@@ -7,7 +7,7 @@ export class ConstantsService {
   root_url: string = 'http://api-abs.azurewebsites.net/'; 
   //root_url: string = 'http://localhost:53628/';
 
-  DocumentTypes: Array<Object> = [];
+  DocumentTypes: Array<Object> = []; 
 
   constructor() { 
       //this._auth.apiGet()
